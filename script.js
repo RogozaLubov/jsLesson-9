f301ce05e5460da61bbc4b12face2e0cdd189273
Rogoza Lyuba <lyubashka22rogoza@gmail.com>
2021-09-26 16:48:07 +0300

    add task 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//task 1
 let arr = [1,2,3];
 
 function func (elem){
@@ -7,3 +8,22 @@ function func (elem){
 }
 
 func(arr);
+//task2
+
+function getSum (arr, sum){
+
+    sum = sum + arr.splice(0,1)[0];
+
+    if(arr.length !== 0) getSum(arr, sum);
+    else console.log (sum);
+}
+
+let h = [1, 2, 3];
+let s = 0;
+getSum(h, s);
+//task 3
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
